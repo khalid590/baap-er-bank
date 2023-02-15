2951e8224d5f803073685bc8499227c1e11a14da
unknown <khalidwasif590@gmail.com>
2023-02-16 01:54:27 +0600

    final submission

--- js/withdraw.js
+++ js/withdraw.js
@@ -9,5 +9,15 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    const newBalanceTotal= previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    withdrawField.value = ' ';
 })
