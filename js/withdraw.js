fe5f19c8249e978d77994b350d1f033fc8880237
unknown <khalidwasif590@gmail.com>
2023-02-16 01:39:48 +0600

    first commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,5 +3,11 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdrawFieldString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawFieldString);
+
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
     
 })
